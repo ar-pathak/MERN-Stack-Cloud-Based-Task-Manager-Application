4cc87050aa300241c354ae85a4fd77e25ce010ea
ar-pathak <pathakarsan@gmail.com>
2025-12-21 10:38:30 +0530

    feat(server): Update server dependencies and configuration

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
+require("dotenv").config();
 
 const { userAuth } = require('./middleware/auth')
+const connectDB = require('./config/database')
 
 
 const app = express();
@@ -10,14 +12,18 @@ app.get('/hello/:id', userAuth, (req, res) => {
     res.status(200).json({ "success": true })
 })
 
+
+connectDB()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`server are Listening on port ${port}`)
+            console.log("DB connected successfully")
+        })
+    })
+
 app.use('/', (err, req, res, next) => {
     if (err) {
         res.status(500).send("something went wrong");
     }
     res.send("Hello world");
-})
-
-
-app.listen(port, () => {
-    console.log(`server are Listening on port ${port}`)
 })
